eec6958aa829b4b5714e74bbe90a7c63fc2aba4b
bencoll <bwcollins97@gmail.com>
2024-11-02 19:14:48 +1100

    chore: add prettier

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -1,9 +1,10 @@
-import { Alert } from "flowbite-react";
+import { Alert } from 'flowbite-react';
 
 export default function Recipes() {
   return (
     <main>
       <Alert color="info">Test</Alert>
+      <p>Hey Bandi</p>
     </main>
   );
 }
